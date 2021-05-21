cda7a8a33327f360732c17be1f508a8fe86fa274
poohyhy <poohyhy98@gmail.com>
2021-05-21 14:15:49 +0900

    "210521"

--- tel.c
+++ tel.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include "tel.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	
+	if (argc == 1) {
+		tel_how();
+		return 0;
+	}
+	if else (argc > 1) {
+		switch (argv[1]) {
+			case "-a" : 
+				tel_add();
+				break;
+			case "-d" : 
+				tel_del();
+				break;
+			case "-l" : 
+				tel_print();
+				break;
+			default :
+				tel_search();
+		}
+	}
+	return 0;
 }
